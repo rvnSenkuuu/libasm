b936b0f360bfa499ae651c05907daf0c99b49d70
rvnSenkuuu <tolu_48@hotmail.fr>
2025-07-18 14:00:04 +0200

    misc: change main for test purposes

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: tkara2 <tkara2@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/14 15:29:18 by tkara2            #+#    #+#             */
-/*   Updated: 2025/07/18 12:24:36 by tkara2           ###   ########.fr       */
+/*   Updated: 2025/07/18 13:58:56 by tkara2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -189,11 +189,46 @@ void	test_ft_read(void)
 
 int	main(void)
 {
-	test_ft_strlen();
-	test_ft_strcmp();
-	test_ft_strcpy();
-	test_ft_strdup();
-	test_ft_write();
-	test_ft_read();
+	char	buffer[2] = {0};
+
+	while (1) {
+		fprintf(stdout, "Available function are: \n");
+		fprintf(stdout, "1- ft_strlen \n");
+		fprintf(stdout, "2- ft_strcmp\n");
+		fprintf(stdout, "3- ft_strcpy\n");
+		fprintf(stdout, "4- ft_strdup\n");
+		fprintf(stdout, "5- ft_write\n");
+		fprintf(stdout, "6- ft_read\n");
+		write(STDOUT_FILENO, ">", sizeof(char));
+		scanf("%2s", buffer);
+
+		int user_input = atoi(buffer);
+		switch (user_input) {
+			case 1:
+				test_ft_strlen();
+				break;
+			case 2:
+				test_ft_strcmp();
+				break;
+			case 3:
+				test_ft_strcpy();
+				break;
+			case 4:
+				test_ft_strdup();
+				break;
+			case 5:
+				test_ft_write();
+				break;
+			case 6:
+				test_ft_read();
+				break;
+			default:
+				fprintf(stdout, "Invalid input\n");
+				fflush(stdout);
+				exit(1);
+		}
+		fflush(stdout);
+		write(STDOUT_FILENO, "\n", sizeof(char));
+	}
 	return 0;
 }
